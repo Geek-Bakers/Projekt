437f64b7d7d24ac73a522f2841ef0b42757eb041
Andrzejek12 <zoladowski.lukasz@gmail.com>
2021-04-26 17:11:34 +0200

    add DB

---
+++ Projekt/Models/Resource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt.Models
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int NumberOfWeek { get; set; }
+        public int NumberOfFile { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Projekt/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string TelNumber { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+        public virtual List<Resource> Resources { get; set; }
+    }
+}
