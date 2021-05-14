77d53ca77be5fadd4b8d4dc44a1ef03da64def59
vvoyciech <wopo96@gmail.com>
2021-05-14 14:30:06 +0200

    bugfix 2

--- Projekt/Startup.cs
+++ Projekt/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SDBWebAPI.Models;
+using Projekt.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
