1792623db7978acc9221a36066503fab53afd9b8
Konrad Mielnik <konrad_mielnik@wp.pl>
2021-05-14 15:04:54 +0200

    Revert "bug fixed"
    This reverts commit 90bee78e45fa36e03e702325ed2958bf6d320112.

--- Projekt/Models/DbContext.cs
+++ Projekt/Models/DbContext.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Projekt.Models
+namespace SDBWebAPI.Models
 {
     public class SDBContext : DbContext
     {
