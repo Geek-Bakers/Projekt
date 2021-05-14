6eb99c1fcb8dc5ebd75c5ba866dfeadfef18938a
vvoyciech <wopo96@gmail.com>
2021-05-14 14:56:20 +0200

    Added DTO, Interfaces and Implementations

---
+++ Projekt/Services/Interfejsy/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Projekt.Models;
+using Projekt.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt.Services.Interfejsy
+{
+    public interface IUserService
+    {
+        ResponseDTO Register(User user);
+        ResponseDTO Login(string email, string password);
+        UsersDTO GetAllUser();
+    }
+}
