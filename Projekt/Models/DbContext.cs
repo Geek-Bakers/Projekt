90bee78e45fa36e03e702325ed2958bf6d320112
Konrad Mielnik <konrad_mielnik@wp.pl>
2021-05-14 12:40:05 +0200

    bug fixed

--- Projekt/Models/DbContext.cs
+++ Projekt/Models/DbContext.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SDBWebAPI.Models
+namespace Projekt.Models
 {
     public class SDBContext : DbContext
     {
